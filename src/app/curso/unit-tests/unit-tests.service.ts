eeb59bf78fa9ccfdbd721d5c3235ff0758958c54
Rodrigo Freitas <rodrigo.d.freitas@outlook.com>
2023-07-10 20:48:32 -0300

    Angular Unit Testing

--- src/app/curso/unit-tests/unit-tests.service.ts
+++ src/app/curso/unit-tests/unit-tests.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { RdfreitasService } from "src/app/services/rdfreitasService";
+import { LoggerService } from "./logger.service";
 
 
 @Injectable({
@@ -7,15 +8,15 @@ import { RdfreitasService } from "src/app/services/rdfreitasService";
 })
 export class UnitTestsService {
 
-    constructor() {
-
-    }
+    constructor(private logger: LoggerService) {}
 
     add(n1: number, n2: number) {
+        this.logger.log('Método de adição chamado');
         return n1 + n2;
     }
 
     subtract(n1: number, n2: number){
+        this.logger.log('Método de subtração chamado');
         return n1 - n2;
     }
 }
